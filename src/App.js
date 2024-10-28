666ebe35bc65c003e21aa872f67e08b14ebcf64f
Aditya Shibu <as2397@hw.ac.uk>
2024-10-28 23:22:58 +0400

    Added data and imported components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { LineGraph } from "./components/Line";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LineGraph />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/test_data.js
@@ -0,0 +1,19 @@
+export const lineChartData = {
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  datasets: [
+    {
+      label: "First dataset",
+      data: [33, 53, 85, 41, 44, 65],
+      fill: true,
+      backgroundColor: "rgba(75,192,192,0.2)",
+      borderColor: "rgba(75,192,192,1)",
+    },
+    {
+      label: "Second dataset",
+      data: [33, 25, 35, 51, 54, 76],
+      fill: true,
+      backgroundColor: "rgba(255, 99, 132, 0.2)",
+      borderColor: "rgba(255, 99, 132, 1)",
+    },
+  ],
+};
